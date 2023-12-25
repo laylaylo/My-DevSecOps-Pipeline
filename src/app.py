d2374765c229823833eb3d74370b7e9088680f63
laylaylo <74381463+laylaylo@users.noreply.github.com>
2023-12-25 04:40:57 +0300

    fix pylint issues

--- src/app.py
+++ src/app.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 def index():
     """Function docstring: Explain what this function does."""
     try:
-	    host_name = socket.gethostname()
-	    host_ip = socket.gethostbyname(host_name)
-	    return render_template('index.html', hostname=host_name, ip=host_ip)
-	except SpecificException as e:  # Replace SpecificException with the actual exception you expect
-	    # Log the exception if needed
-	    return render_template('error.html')
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return render_template('index.html', hostname=host_name, ip=host_ip)
+    except SpecificException as e:  # Replace SpecificException with the actual exception you expect
+        # Log the exception if needed
+        return render_template('error.html')
 
 
 if __name__ == "__main__":
