c1d48316451437922a8db5b6be260790673d1070
laylaylo <74381463+laylaylo@users.noreply.github.com>
2023-12-25 04:44:10 +0300

    fix pylint issues

--- src/app.py
+++ src/app.py
@@ -12,10 +12,9 @@ def index():
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
         return render_template('index.html', hostname=host_name, ip=host_ip)
-    except SpecificException as e:  # Replace SpecificException with the actual exception you expect
-        # Log the exception if needed
+    except socket.gaierror as e:  # Use Exception or a more specific one
+        print(f"Error: {e}")  # Example of using the exception variable
         return render_template('error.html')
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
